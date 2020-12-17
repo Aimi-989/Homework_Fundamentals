948bb7ee14576ee4f41288b5904bc5a6df4ec267
Aimi <email@gmail.com>
2020-12-17 21:51:41 +0200

    git push origin master

--- src/loops/Task_15.java
+++ src/loops/Task_15.java
@@ -2,23 +2,21 @@ package loops;
 
 import java.util.Scanner;
 
-public class Task_14 {
+public class Task_15 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter numbers: ");
+        //int number = scanner.nextInt();
 
         int result = 0;
         int number = -1;
 
         while (number != 0) {
             number = scanner.nextInt();
-           // if () {
 
             //Тут выполните условия задачи (сумма всех чисел в 14
             // или сумма самого большого и самого маленького и их среднее арифметическое в 15)
         }
         System.out.println(result);
-
-
     }
 }
